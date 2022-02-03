100616cc31d33179698568d1d3af0160dd38cb01
Your Name <you@example.com>
2022-02-04 00:00:30 +0530

    postgres config

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['e-shop-env.eba-hbgic2d4.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['e-shop-env.eba-hbgic2d4.us-west-2.elasticbeanstalk.com',"*"]
 
 
 # Application definition
